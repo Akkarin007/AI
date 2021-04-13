1164aa359d455ad3b029a7bb22ccc0add4fa1fb9
Akkarin <isyzeybek@outlook.com>
2021-04-13 12:37:30 +0200

    done

--- Aufgabe_2_puzzel_8/src/puzzle8/Puzzle8.java
+++ Aufgabe_2_puzzel_8/src/puzzle8/Puzzle8.java
@@ -4,25 +4,25 @@ import java.util.Deque;
 
 /**
  * Hauptprogramm für 8-Puzzle-Problem.
+ *
  * @author Ihr Name
  */
 public class Puzzle8 {
-	
-	public static void main(String[] args) {
-		//Board b = new Board(); // Loesbares Puzzle b zufällig genrieren.
-		Board b = new Board(new int[]{7, 2, 4, 5, 0, 6, 8, 3, 1});
-		System.out.println(b);
 
+    public static void main(String[] args) {
+        Board b = new Board(); // Loesbares Puzzle b zufällig genrieren.
+        //Board b = new Board(new int[]{7, 2, 4, 5, 0, 6, 8, 3, 1});
+        System.out.println(b);
 
-		if(b.parity()) {
-			Deque<Board> res = A_Star.aStar(b);
-			int n = res == null ? -1 : res.size();
-			System.out.println("Anz.Zuege: " + n + ": " + res);
+        while (!b.parity()) b = new Board();
+        if (b.parity()) {
+            Deque<Board> res = A_Star.aStar(b);
+            int n = res == null ? -1 : res.size();
+            System.out.println("Anz.Zuege: " + n + ": " + res);
 
-			res = IDFS.idfs(b);
-			n = res == null ? -1 : res.size();
-			System.out.println("Anz.Zuege: " + n + ": " + res);
-		}
-	}
-
+            res = IDFS.idfs(b);
+            n = res == null ? -1 : res.size();
+            System.out.println("Anz.Zuege: " + n + ": " + res);
+        }
+    }
 }
