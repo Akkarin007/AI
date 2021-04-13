f823a29b9ba1d54f0fc375ed612b4e13f0fda92c
Akkarin <isyzeybek@outlook.com>
2021-04-13 12:31:16 +0200

    almost done

--- Aufgabe_2_puzzel_8/src/puzzle8/Puzzle8.java
+++ Aufgabe_2_puzzel_8/src/puzzle8/Puzzle8.java
@@ -17,7 +17,7 @@ public class Puzzle8 {
 		if(b.parity()) {
 			Deque<Board> res = A_Star.aStar(b);
 			int n = res == null ? -1 : res.size();
-			//System.out.println("Anz.Zuege: " + n + ": " + res);
+			System.out.println("Anz.Zuege: " + n + ": " + res);
 
 			res = IDFS.idfs(b);
 			n = res == null ? -1 : res.size();
