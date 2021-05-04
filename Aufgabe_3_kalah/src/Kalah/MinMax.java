f6b5b0e2c29e2c3114d79386276601947ad8507a
Akkarin <isyzeybek@outlook.com>
2021-05-04 11:24:15 +0200

    a3 init

--- Aufgabe_3_kalah/src/Kalah/MinMax.java
+++ Aufgabe_3_kalah/src/Kalah/MinMax.java
@@ -1,31 +1,53 @@
 package Kalah;
 
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 
 public class MinMax {
 
     public MinMax(KalahBoard board) {
+        KalahBoard move = miniMax(board, 5);
+        System.out.println("using move: " + move);
 
     }
 
-    public int min(KalahBoard board) {
-        if (board.isFinished()){
+
+    public KalahBoard miniMax(KalahBoard board, int depth) {
+
+        int v = Integer.MIN_VALUE;
+        List<KalahBoard> boards = board.possibleActions();
+        KalahBoard action = boards.get(0);
+        for (KalahBoard b : board.possibleActions()) {
+            int temp = v;
+            v = Integer.max(v, min(b, depth - 1));
+            if (temp != v){
+                action = b;
+            }
+        }
+        return action;
+    }
+
+
+    public int min(KalahBoard board, int depth) {
+        if (depth == 0 || board.isFinished()){
             return board.getBKalah();
         }
         int v = Integer.MAX_VALUE;
         for (KalahBoard b : board.possibleActions()) {
-            v = Integer.min(v, max(b));
+            v = Integer.min(v, max(b, depth - 1));
         }
         return v;
     }
 
-    public int max(KalahBoard board) {
-        if (board.isFinished()){
+    public int max(KalahBoard board, int depth) {
+
+        if (depth == 0 || board.isFinished()){
             return board.getAKalah();
         }
         int v = Integer.MIN_VALUE;
         for (KalahBoard b : board.possibleActions()) {
-            v = Integer.max(v, min(b));
+            v = Integer.max(v, min(b, depth - 1));
         }
         return v;
     }
