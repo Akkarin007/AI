3b7b51c03288afa54674b6b0fbbfacf2e9398cf2
Akkarin <isyzeybek@outlook.com>
2021-06-01 11:22:41 +0200

    almost done

--- Aufgabe_4_ML/titanicML.py
+++ Aufgabe_4_ML/titanicML.py
@@ -1,41 +1,88 @@
 
-
 # Hinweise zum Einlesen
 import keras
 import matplotlib.pyplot as plt
 plt.style.use('default')
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import seaborn as sns
+
+
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report,confusion_matrix,accuracy_score
 from keras import Sequential
 from keras.layers import Dense, Activation
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 
-train_val = pd.read_csv('titanic_data/train.csv')
+
+train = pd.read_csv('titanic_data/train.csv')
 # Hinweise um die Accuracy zu berechnen
 from sklearn.metrics import accuracy_score
 
 
-length = int(len(train_val) * 0.8)
-train_dataset = train_val[:length]
-validation_dataset = train_val[length:]
+#length = int(len(train_val) * 0.8)
+#train_dataset = train_val[:length]
+#validation_dataset = train_val[length:]
 
-#print(train_dataset.head())
-pd.crosstab(train_dataset['Pclass'], train_dataset['Survived'])
+train.drop('Cabin',axis=1,inplace=True)
+train['Age'].fillna((train['Age'].median()), inplace=True)
 
-survived_value_counts = train_dataset.groupby('Pclass').Survived.value_counts()
-survived_percentage = train_dataset[['Pclass', 'Survived']].groupby(['Pclass'], as_index=False).mean()
+sns.set_style('whitegrid')
+sns.countplot(x='Survived',data=train,palette='RdBu_r')
 
-print(survived_value_counts)
-print(survived_percentage)
 
-model = Sequential()
-model.add(Dense(64, activation='relu', input_dim=50)) #input shape of 50
-model.add(Dense(28, activation='relu')) #input shape of 50
-model.add(Dense(10, activation='softmax'))
-model.compile(loss='categorical_crossentropy', optimizer='adadelta', metrics=['accuracy'])
 
-history = model.fit(train_dataset, train_dataset, batch_size=128, epochs=10, verbose=2, validation_data=(validation_dataset, validation_dataset))
+sns.set_style('darkgrid')
+sns.countplot(x='Survived',hue='Sex',data=train,palette='ocean')
 
-plt.title("blood pressure vs age")
-plt.xlabel("x (age)")
-plt.ylabel("y (sbp)")
-plt.plot(history)
-plt.show()
+sns.set_style('whitegrid')
+sns.countplot(x='Survived',hue='Pclass',data=train,palette='winter')
+
+sns.distplot(train['Age'].dropna(),kde=False,color='darkred',bins=20)
+
+sns.countplot(x='SibSp',data=train,palette='ocean')
+
+sns.countplot(x='Parch',data=train,palette='ocean')
+
+
+
+plt.figure(figsize=(12, 7))
+sns.boxplot(x='Pclass',y='Age',data=train,palette='winter')
+
+
+
+train.info()
+
+# create the dummy variables and drop one column as there is no need of 2 columns in order to differentiate the values.
+sex = pd.get_dummies(train['Sex'],drop_first=True)
+# similarly for this colimn as well. If there are n dummy columns, consider n-1
+embark = pd.get_dummies(train['Embarked'],drop_first=True)
+
+
+
+train.drop(['Sex','Embarked','Name','Ticket'],axis=1,inplace=True)
+train = pd.concat([train,sex,embark],axis=1)
+
+
+
+train.head()
+X = train.drop("Survived",axis=1)
+y = train['Survived']
+#X_train, X_test, y_train, y_test = train_test_split(train.drop('Survived',axis=1),train['Survived'], test_size=0.20,random_state=5)
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.20,random_state=5)
+
+# create an instance
+logmodel = LogisticRegression()
+# pass the values and build the model
+logmodel.fit(X_train,y_train)
+
+
+
+# preditcing the test models
+predictions = logmodel.predict(X_test)
+
+
+
+
+
+print(classification_report(y_test,predictions))
+
